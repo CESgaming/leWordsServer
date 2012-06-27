0a569fc4fe8faf1903f1b488475eb35c47e1a124
pcnx <pecoll@web.de>
2012-06-27 15:11:09 +0200

    Reduced CPU load

--- src/GameManager.java
+++ src/GameManager.java
@@ -81,7 +81,11 @@ public class GameManager  extends Thread {
 		startTime = System.currentTimeMillis();
 		while(running)
 		{
-			
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 			switch(GAMESTATE)
 			{
 			case 0:
